933ee504b9fa4b139a23ca49ca02aba98c2f5553
Alexander Gavrilenko <agavrilenko@nm.ru>
2013-07-17 14:40:06 +0400

    Add Next button, and some real question.

---
+++ src/ru/gavr/android/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package ru.gavr.android.geoquiz;
+
+/**
+ * @author Alexander Gavrilenko
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        this.mQuestion = question;
+        this.mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
