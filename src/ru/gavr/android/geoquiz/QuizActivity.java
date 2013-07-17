d6601aaaa7b18c607bdc630220d135094e0d2599
Alexander Gavrilenko <agavrilenko@nm.ru>
2013-07-17 16:18:21 +0400

    Add a listener to the TextView

--- src/ru/gavr/android/geoquiz/QuizActivity.java
+++ src/ru/gavr/android/geoquiz/QuizActivity.java
@@ -33,6 +33,13 @@ public class QuizActivity extends Activity {
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         updateQuestion();
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
 
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
